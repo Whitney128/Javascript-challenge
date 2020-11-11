e93cdf1163156a2da7653d4848977bfeee725675
Whitney128 <White.Whitney543@gmail.com>
2020-11-10 17:49:09 -0700

    update readability

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,17 @@
+//pull data.js
 var tableData = data;
 console.log(tableData);
 
+//reference table body
 var tbody = d3.select("tbody");
 
+//ufo sightings info for columns
 tableData.forEach(function(ufosighting) {
 	console.log(ufosighting);
+	// Append one table row for each UFO Sighting
 	var row = tbody.append("tr");
 
+	//adding object.entries for sighting values
 	Object.entries(ufosighting).forEach(function([key, value]) {
 		console.log(key, value);
 		var cell = row.append("td")
@@ -22,11 +27,12 @@ function runEnter() {
 
 	tbody.html('');
 
+	//add select feature
 	var inputelement = d3.select("#datetime");
 	var inputvalue = inputelement.property("value");
 	console.log(inputvalue);
-	//console.log(ufosighting);
 
+	//filter datetime
 	filteredData = tableData.filter(ufosighting => ufosighting.datetime === inputvalue);
 	console.log(filteredData);
 
